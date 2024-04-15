02246edb5f904c233edfca14313ede58e3f9419b
eeriez <112532572+eeriez@users.noreply.github.com>
2024-04-15 19:56:04 +0300

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -8,11 +8,11 @@ my_list[2] = 'grape'
 print(my_list)
 
 
-my_dict = {'apple' : 'яблоко', 'orange' : 'апельсин', 'banana' : 'банан', 'pomergranate' : 'гранат', 'pear' : 'груша', 'tangerine' : 'мандарин'}
+my_dict = {'apple': 'яблоко', 'orange': 'апельсин', 'banana': 'банан', 'pomergranate': 'гранат', 'pear': 'груша', 'tangerine': 'мандарин'}
 
 print(my_dict)
 print(my_dict['banana'])
 my_dict['pear'] = 'пеар'
-my_dict.update({'tangerine' : 'тангерине',
-                'orange' : 'оранге'})
+my_dict.update({'tangerine': 'тангерине',
+                'orange': 'оранге'})
 print(my_dict)
