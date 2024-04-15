cf1bab3d8b1b9ea9ea15147f01da48be63983cc1
eeriez <112532572+eeriez@users.noreply.github.com>
2024-04-15 19:35:30 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,18 @@
+my_list = ['apple', 'orange', 'banana', 'pomergranate', 'pear', 'tangerine']
+
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
+my_list[2] = 'grape'
+print(my_list)
+
+
+my_dict = {'apple' : 'яблоко', 'orange' : 'апельсин', 'banana' : 'банан', 'pomergranate' : 'гранат', 'pear' : 'груша', 'tangerine' : 'мандарин'}
+
+print(my_dict)
+print(my_dict['banana'])
+my_dict['pear'] = 'пеар'
+my_dict.update({'tangerine' : 'тангерине',
+                'orange' : 'оранге'})
+print(my_dict)
