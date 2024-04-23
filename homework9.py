2491f50a08754d171a14e7cd02bdccd5594ccb30
eeriez <112532572+eeriez@users.noreply.github.com>
2024-04-23 04:05:08 +0300

    Add files via upload

---
+++ homework9.py
@@ -0,0 +1,9 @@
+def print_params(param):
+    print(str(param)*2)
+    # или print(param, param)?
+
+print_params(2)
+print_params('аа')
+print_params((1, 2, 3))
+print_params([1, 5, 6, 7])
+print_params({'А': 'а', 'Б': 'б'})
