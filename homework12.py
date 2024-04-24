04ecd8b7bd8653359d88683b3b384d95cc4498f7
eeriez <112532572+eeriez@users.noreply.github.com>
2024-04-25 01:13:22 +0300

    Add files via upload

---
+++ homework12.py
@@ -0,0 +1,14 @@
+def test(n, *args, sum):
+    print(n, *args, sum)
+    print()
+
+test(2, 4, 5, 6, 7, 8, 9, sum=67890)
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        fact = n * factorial(n-1)
+        return fact
+
+print(factorial(10))
